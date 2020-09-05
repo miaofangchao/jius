8632fb3474343980f2eebe8e6b7089377f243126
miaofangchao <mssking@qq.com>
2020-09-05 11:17:13 +0800

    至代理查询

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router'
+import vueTap from 'v-tap'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios,axios)
+Vue.use(vueTap)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
